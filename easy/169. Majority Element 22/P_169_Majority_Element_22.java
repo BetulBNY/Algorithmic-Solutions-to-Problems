86b029b3b4c670f4e3fb735208d1e5d3ba17f7dd
Betül Nur Yıldırım <107762579+BetulBNY@users.noreply.github.com>
2024-02-27 15:42:37 +0300

    Update P_169_Majority_Element_22.java

--- easy/169. Majority Element 22/P_169_Majority_Element_22.java
+++ easy/169. Majority Element 22/P_169_Majority_Element_22.java
@@ -27,11 +27,34 @@ import java.util.Arrays;
 
 // 1) Sort all elements in the array. 
 // 2) Middle element of array gives majority element.
+
+// 1st Way Time 5ms - Beats 56.64% // Space 53.66MB - Beats 5.24%
 public class P_169_Majority_Element_22 {
         public static int majorityElement(int[] nums) {
             Arrays.sort(nums);           
         return nums[nums.length/2];
     }
+        
+// 2nd Way Time 1ms - Beats 99.69% // Space 53.42MB - Beats 5.24%
+public static int majorityElement2(int[] nums) {
+        int counter = 0;
+        int majoel = 0;
+            
+        for(int num : nums){
+                if(counter == 0){
+                    majoel = num;
+                    counter++;
+                }
+                else if(num == majoel){
+                    counter++;
+                }
+                else{
+                    counter--;
+                }
+            }
+            return majoel;
+}
+        
         public static void main(String[] args) {
         int arr[] = {3,2,3};
         System.out.println(majorityElement(arr));
