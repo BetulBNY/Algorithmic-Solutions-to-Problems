a2d2a93736900f98a937a151516bec9f6c4d5cc4
BetulBNY <betulbny@gmail.com>
2025-12-12 15:51:20 +0300

    2)Evaluate_Reverse_Polish_Notation Stack Question

---
+++ 3)Stack/2)Evaluate_Reverse_Polish_Notation.py
@@ -0,0 +1,64 @@
+# You are given an array of strings tokens that represents an arithmetic expression in a Reverse Polish Notation.
+# Evaluate the expression. Return an integer that represents the value of the expression.
+#
+# Note that:
+# The valid operators are '+', '-', '*', and '/'.
+# Each operand may be an integer or another expression.
+# The division between two integers always truncates toward zero.
+# There will not be any division by zero.
+# The input represents a valid arithmetic expression in a reverse polish notation.
+# The answer and all the intermediate calculations can be represented in a 32-bit integer.
+
+# Example 1:
+# Input: tokens = ["2","1","+","3","*"]
+# Output: 9
+# Explanation: ((2 + 1) * 3) = 9
+
+# Example 2:
+# Input: tokens = ["4","13","5","/","+"]
+# Output: 6
+# Explanation: (4 + (13 / 5)) = 6
+
+# Example 3:
+# Input: tokens = ["10","6","9","3","+","-11","*","/","*","17","+","5","+"]
+# Output: 22
+# Explanation: ((10 * (6 / ((9 + 3) * -11))) + 17) + 5
+# = ((10 * (6 / (12 * -11))) + 17) + 5
+# = ((10 * (6 / -132)) + 17) + 5
+# = ((10 * 0) + 17) + 5
+# = (0 + 17) + 5
+# = 17 + 5
+# = 22
+
+
+# Çözüm Mantığı
+# Sayı gelirse → stacke pushla
+# Operatör gelirse → stack’ten 2 eleman pop → işlem yap → sonucu push
+
+tokens1 = ["2","1","+","3","*"]
+tokens2 = ["4","13","5","/","+"]
+tokens3 = ["10","6","9","3","+","-11","*","/","*","17","+","5","+"]
+def evalRPN(tokens:list) -> int:
+    my_stack = []
+    not_num = ['+', '-', '*', '/']
+    for token in tokens:
+        if token not in not_num:
+            my_stack.append(int(token))
+        else:
+            num1 = my_stack.pop()
+            print("num1: ",num1)
+            num2 = my_stack.pop()
+            print("num2: ", num2)
+            result = str(num2) + token + str(num1)
+            print("result:", result)
+            if token == "/":
+                my_stack.append(int(float(num2) / num1))
+            elif token == "+":
+                my_stack.append(num2+num1)
+            elif token == "-":
+                my_stack.append(num2 - num1)
+            elif token == "*":
+                my_stack.append(int(num2 * num1))
+    return my_stack[0]
+
+print(evalRPN(tokens3))
